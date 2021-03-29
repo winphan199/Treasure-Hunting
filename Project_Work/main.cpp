c2a33a575c1239d5fccd4a0b324e83a8049f95ed
WIN <winphan199@gmail.com>
2021-03-29 22:27:38 +0700

    fixed bug release

--- Project_Work/main.cpp
+++ Project_Work/main.cpp
@@ -1,8 +1,9 @@
-#include "Dungeon.hpp"
 #include "GamePlay.h"
 vector<string> Logger::_records;
 int Gem::_gemCount = 0;
 int main() {
     GamePlay::gameLoop();
+    getchar();
+    getchar();
     return 0;
 }
